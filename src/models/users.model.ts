e7cf61817e9f056e55d8dd13ba585b770b45e5b5
tormelo <23294699+tormelo@users.noreply.github.com>
2022-12-15 15:43:50 -0300

    add getByCredentials to users.model

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,5 +1,6 @@
-import { ResultSetHeader } from 'mysql2/promise';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import IUser from '../interfaces/IUser';
+import IUserCredentials from '../interfaces/IUserCredentials';
 import connection from './connection';
 
 async function register(user: IUser): Promise<ResultSetHeader> {
@@ -14,6 +15,18 @@ async function register(user: IUser): Promise<ResultSetHeader> {
   return result;
 }
 
+async function getByCredentials(userCredentials: IUserCredentials): Promise<IUser[]> {
+  const { username, password } = userCredentials;
+
+  const query = 'SELECT * FROM Trybesmith.users WHERE username = ? AND password = ?;';
+  const values = [username, password];
+
+  const [users] = await connection.execute<RowDataPacket[]>(query, values);
+  
+  return users as IUser[];
+}
+
 export default {
   register,
+  getByCredentials,
 };
