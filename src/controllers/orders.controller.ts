f8e56182e6eb15671220b4a47970fc4532b19e4b
tormelo <23294699+tormelo@users.noreply.github.com>
2022-12-15 18:04:57 -0300

    add register function to orders.controller

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from 'express';
+import IOrder from '../interfaces/IOrder';
 import ordersService from '../services/orders.service';
+import mapError from '../utils/errorMap';
+
+async function register(req: Request, res:Response) {
+  console.log(req.body);
+  const { payload, productsIds } = req.body;
+  const order: IOrder = { userId: payload.id, productsIds };
+
+  const { type, message } = await ordersService.register(order);
+
+  if (type) return res.status(mapError(type)).json({ message });
+
+  return res.status(201).json(message);
+}
 
 async function getAll(req: Request, res:Response) {
   const { message } = await ordersService.getAll();
@@ -7,5 +21,6 @@ async function getAll(req: Request, res:Response) {
 }
 
 export default {
+  register,
   getAll,
 };
--- src/routers/orders.router.ts
+++ src/routers/orders.router.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import ordersController from '../controllers/orders.controller';
+import validateToken from '../middlewares/validateToken';
 
 export const router = express.Router();
 
 router.get(
   '/',
   ordersController.getAll,
+);
+
+router.post(
+  '/',
+  validateToken,
+  ordersController.register,
 );
 
 export default router;
