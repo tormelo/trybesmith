61de0706b94f41a2438815c81eaa2429bc93e987
tormelo <23294699+tormelo@users.noreply.github.com>
2022-12-15 18:03:35 -0300

    add register function to orders.service

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,4 +1,22 @@
+import IOrder from '../interfaces/IOrder';
 import ordersModel from '../models/orders.model';
+import productsModel from '../models/products.model';
+import { validateOrderBody } from './validations/inputValidations';
+
+async function register(order: IOrder) {
+  const error = validateOrderBody(order);
+  if (error.type) return error;
+
+  const { productsIds, userId } = order;
+  
+  const { insertId: orderId } = await ordersModel.register(userId);
+
+  await Promise.all(productsIds.map(async (productId) => {
+    await productsModel.update(productId, orderId);
+  }));
+
+  return { type: null, message: { userId, productsIds } };
+}
 
 async function getAll() {
   const orders = await ordersModel.getAll();
@@ -6,5 +24,6 @@ async function getAll() {
 }
 
 export default {
+  register,
   getAll,
 };
