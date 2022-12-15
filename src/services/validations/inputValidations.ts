28de2fb314892a6b481bd739a116f41cc951add9
tormelo <23294699+tormelo@users.noreply.github.com>
2022-12-15 15:05:57 -0300

    refactor validations to use generics

--- src/services/validations/inputValidations.ts
+++ src/services/validations/inputValidations.ts
@@ -1,9 +1,10 @@
+import { ObjectSchema } from 'joi';
 import IProduct from '../../interfaces/IProduct';
 import IUser from '../../interfaces/IUser';
 import { productBodySchema, userBodySchema } from './schemas';
 
-export function validateProductBody(productBody: IProduct) {
-  const { error } = productBodySchema.validate(productBody);
+function validateSchema<T>(data: T, schema: ObjectSchema) {
+  const { error } = schema.validate(data);
   if (error) {
     const { message } = error;
     if (message.includes('required')) return { type: 'REQUIRED_FIELD', message };
@@ -13,13 +14,10 @@ export function validateProductBody(productBody: IProduct) {
   return { type: null, message: '' };
 }
 
-export function validateUserBody(userBody: IUser) {
-  const { error } = userBodySchema.validate(userBody);
-  if (error) {
-    const { message } = error;
-    if (message.includes('required')) return { type: 'REQUIRED_FIELD', message };
-    return { type: 'INVALID_VALUE', message: error.message };
-  }
+export function validateProductBody(productBody: IProduct) {
+  return validateSchema<IProduct>(productBody, productBodySchema);
+}
 
-  return { type: null, message: '' };
+export function validateUserBody(userBody: IUser) {
+  return validateSchema<IUser>(userBody, userBodySchema);
 }
